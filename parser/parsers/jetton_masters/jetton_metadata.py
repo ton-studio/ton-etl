4356d95293bca03bc216680fc1cd55ad063c580d
Aleksandr Kaekhtin <kaehtin.aa@gmail.com>
2025-02-12 16:11:33 +0300

    Update item metadata and jetton metadata parsers

--- parser/parsers/jetton_masters/jetton_metadata.py
+++ parser/parsers/jetton_masters/jetton_metadata.py
@@ -156,6 +156,8 @@ class JettonMastersMetadataParser(Parser):
                 def update_metadata(index, obj, key, prev, source=METADATA_OFFCHAIN):
                     if sources[index] == "" and obj.get(key, None):
                         value = obj.get(key, None)
+                        if isinstance(value, str):
+                            value = value.replace("\x00", "")
                         logger.info(f"Using {source} {key}: {value}")
                         sources[index] = source
                         return value
