bba76d8360c59d4e223286d22da7c68b561bab4e
Aleksandr Kaekhtin <kaehtin.aa@gmail.com>
2025-06-11 10:15:29 +0300

    Fix put_on_sale and cancel_sale events

--- datalake/airflow/dags/datalake_daily_sync.py
+++ datalake/airflow/dags/datalake_daily_sync.py
@@ -767,7 +767,7 @@ def datalake_daily_sync():
         ), put_on_sale_ranks as (
             select t.block_date, nft_item_address, nft_item_index, nft_collection_address as collection_address,
             nft_owner_address as owner_address,
-            is_init, content_onchain, row_number() over(partition by ni.address order by ni.lt desc) as nft_state_rank, -- from nft_items
+            is_init, content_onchain, row_number() over(partition by ni.address, t.tx_hash order by ni.lt desc) as nft_state_rank, -- from nft_items
             tx_now as timestamp, tx_lt as lt, tx_hash, trace_id, forward_amount, forward_payload, comment,
             nft_sale_contract, sale_type, end_time as sale_end_time, marketplace_address, marketplace_fee_address,
             marketplace_fee, price, asset, royalty_address, royalty_amount, max_bid, min_bid, min_step, query_id, custom_payload
@@ -790,7 +790,7 @@ def datalake_daily_sync():
         ), cancel_sale_ranks as (
             select t.block_date, nft_item_address, nft_item_index, nft_collection_address as collection_address,
             nft_owner_address as owner_address, 
-            is_init, content_onchain, row_number() over(partition by ni.address order by ni.lt desc) as nft_state_rank, -- from nft_items
+            is_init, content_onchain, row_number() over(partition by ni.address, t.tx_hash order by ni.lt desc) as nft_state_rank, -- from nft_items
             tx_now as timestamp, tx_lt as lt, tx_hash, trace_id, forward_amount, forward_payload, comment,
             nft_sale_contract, sale_type, end_time as sale_end_time, marketplace_address, marketplace_fee_address,
             marketplace_fee, price, asset, royalty_address, royalty_amount, max_bid, min_bid, min_step, query_id, custom_payload
