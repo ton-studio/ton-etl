de3fb37f6aa8d8d9cbd352b5524d3ddf8a7cb2a3
maloleg <maloleg2001@gmail.com>
2025-08-09 14:55:19 +0300

    bidask_clmm swap edits query_id, referral_addr added reserves removed new opcodes parsing (swap_v2, callback_v2) added pool validation via code hashes added

--- parser/parsers/message/bidask_swap_clmm.py
+++ parser/parsers/message/bidask_swap_clmm.py
@@ -10,7 +10,9 @@ from pytvm.tvm_emulator.tvm_emulator import TvmEmulator
 
 
 class BidaskClmmSwap(EmulatorParser):
-    BIDASK_FACTORY_ADDRESS = Address('EQAuBZGak9BdkxuCC9gWUsY4Em3jog94BI4eRzX-3_Bidask')
+    BIDASK_POOLS_CODE_HASHES = [
+        "bff2aa0e9e34825cd01d08146844dac8f4aaec41272cfc12e42ca4c1481285c1"
+        ]
 
     def __init__(self, emulator_path):
         EmulatorParser.__init__(self, emulator_path)
@@ -21,13 +23,19 @@ class BidaskClmmSwap(EmulatorParser):
 
     def predicate(self, obj) -> bool:
         # only internal messages sent to the router
-        return obj.get("opcode", None) == Parser.opcode_signed(0x520e4831) and \
+        return obj.get("opcode", None) in [Parser.opcode_signed(0x520e4831), Parser.opcode_signed(0xd3a25890)] and \
             obj.get("direction", None) == "in"
     
-    def validate_pool(self, db: DB, pool_addr: Address): 
-        first_pool_tx = db.get_first_transaction(pool_addr.to_str(is_user_friendly=False))
+    def validate_pool(self, db: DB, pool_state): 
+        try:
+            code_boc_str = pool_state["code_boc"]
+            code_cell_hash = Cell.one_from_boc(code_boc_str)._hash.hex()
+            if code_cell_hash in self.BIDASK_POOLS_CODE_HASHES:
+                return True
+        except Exception as e:
+            logger.info("bidask pool state not found", e)
 
-        return Address(first_pool_tx.get('source', None)) == self.BIDASK_FACTORY_ADDRESS
+        return False
 
     def prepare(self, db: DB):
         EmulatorParser.prepare(self, db)
@@ -39,80 +47,163 @@ class BidaskClmmSwap(EmulatorParser):
         if not tx:
             logger.info(f"Skipping failed tx for {tx_hash}")
             return
-        if not self.validate_pool(db, Address(db.get('destination'), None)):
+        
+        pool_state = Parser.get_account_state_safe(obj.get("destination"), db)
+
+        if not self.validate_pool(db, pool_state):
             logger.warning(f"Skipping invalid pool {obj.get('source', None)}")
             return
 
-        pool_state = Parser.get_account_state_safe(obj.get("destination"), db)
         pool_emulator = self._prepare_emulator(pool_state)
-        
+
         cell = Parser.message_body(obj, db).begin_parse()
-        cell.load_uint(32) # 0x520e4831
-        query_id = cell.load_uint(64)
-        new_current_bin_number = cell.load_uint(32)
-        new_sqrt_p = cell.load_uint(32)
-        from_account = cell.load_bit()
-        amount_x = cell.load_coins()
-        amount_y = cell.load_coins()
-        is_x = cell.load_bit()
-        receiver_address = cell.load_address()
+        op = cell.load_uint(32) # 0x520e4831 pool:swap_success_callback
 
-        logger.info(f"query_id: {query_id}, from_account: {from_account}, amount_x: {amount_x}, amount_y: {amount_y}, is_x: {is_x}, receiver_address: {receiver_address}, from_address: {from_address}")
+        if op == 0x520e4831: # pool:swap_success_callback
+            query_id = cell.load_uint(64)
+            new_current_bin_number = cell.load_uint(32)
+            new_sqrt_p = cell.load_uint(32)
+            from_account = cell.load_bit()
+            amount_x = cell.load_coins()
+            amount_y = cell.load_coins()
+            is_x = cell.load_bit()
+            receiver_address = cell.load_address()
+            additional_data_cell = cell.load_maybe_ref()
+            ref_addr = None
+            if additional_data_cell is not None:
+                data_slice = additional_data_cell.begin_parse()
+                from_address = data_slice.load_address()
+                try:
+                    ref_addr = data_slice.load_address()
+                except Exception as e:
+                    pass
 
-        range_tx = obj
-        for i in range(5): # max 5 reentrancies for swap
-            try:
-                range_swap = Cell.one_from_boc(db.get_parent_message_body(range_tx.get('msg_hash'))).begin_parse()
-            except Exception as e:
-                logger.info("parrent range swap not found", e)
+            logger.info(f"query_id: {query_id}, from_account: {from_account}, amount_x: {amount_x}, amount_y: {amount_y}, is_x: {is_x}, receiver_address: {receiver_address}, from_address: {from_address}")
 
-            swap_op = range_swap.load_uint(32)
-            if swap_op != 0x66210c65:
-                logger.warning(f"Parent message for bidask clmm swap is {swap_op}, expected 0x66210c65 {tx_hash}")
-                range_tx = db.get_parent_message_with_body(range_tx.get('msg_hash'))
-                continue
-            else:
-                break
+            range_tx = obj
+            for i in range(5): # max 5 reentrancies for swap
+                try:
+                    range_swap = Cell.one_from_boc(db.get_parent_message_body(range_tx.get('msg_hash'))).begin_parse()
+                except Exception as e:
+                    logger.info("parrent range swap not found", e)
 
-        input_query_id = range_swap.load_uint(64)
-        input_user_address = range_swap.load_address()
-        input_is_account = range_swap.load_bit()
-        input_is_x = range_swap.load_bit()
-        input_amount = range_swap.load_coins()
-        input_out = range_swap.load_coins()
+                swap_op = range_swap.load_uint(32)
+                if swap_op != 0x66210c65: # range:swap
+                    logger.warning(f"Parent message for bidask clmm swap is {swap_op}, expected 0x66210c65 {tx_hash}")
+                    range_tx = db.get_parent_message_with_body(range_tx.get('msg_hash'))
+                    continue
+                else:
+                    break
 
-        logger.info(f"input_query_id: {input_query_id}, input_is_account: {input_is_account}, input_is_x: {input_is_x}, input_amount: {input_amount}, is_x: {is_x}, input_out: {input_out}")
+            input_query_id = range_swap.load_uint(64)
+            input_user_address = range_swap.load_address()
+            input_is_account = range_swap.load_bit()
+            input_is_x = range_swap.load_bit()
+            input_amount = range_swap.load_coins()
+            input_out = range_swap.load_coins()
 
-        token_x, token_y, = self._execute_method(pool_emulator, 'get_pool_tokens', [], db, {})
-        token_x_address = token_x.load_address()
-        token_y_address = token_y.load_address()
+            logger.info(f"input_query_id: {input_query_id}, input_is_account: {input_is_account}, input_is_x: {input_is_x}, input_amount: {input_amount}, is_x: {is_x}, input_out: {input_out}")
 
-        src_token = [token_x_address, token_y_address][is_x]
-        dst_token = [token_y_address, token_x_address][is_x]
-        output_amount = [amount_x, amount_y][is_x]
+            token_x, token_y, = self._execute_method(pool_emulator, 'get_pool_tokens', [], db, {})
+            token_x_address = token_x.load_address()
+            token_y_address = token_y.load_address()
 
-        src_token_master = db.get_wallet_master(src_token)
-        dst_token_master = db.get_wallet_master(dst_token)
+            src_token = [token_x_address, token_y_address][is_x]
+            dst_token = [token_y_address, token_x_address][is_x]
+            output_amount = [amount_x, amount_y][is_x]
 
-        reserve0, reserve1 = self._execute_method(pool_emulator, 'get_tvl', [], db, {})
+            src_token_master = db.get_wallet_master(src_token)
+            dst_token_master = db.get_wallet_master(dst_token)
 
-        swap = DexSwapParsed(
-            tx_hash=Parser.require(obj.get('tx_hash', None)),
-            msg_hash=Parser.require(obj.get('msg_hash', None)),
-            trace_id=Parser.require(obj.get('trace_id', None)),
-            platform=DEX_BIDASK_CLMM,
-            swap_utime=Parser.require(obj.get('created_at', None)),
-            swap_user=input_user_address,
-            swap_pool=Parser.require(obj.get('destination', None)),
-            swap_src_token=src_token_master,
-            swap_dst_token=dst_token_master,
-            swap_src_amount=input_amount,
-            swap_dst_amount=output_amount,
-            referral_address=None,
-            reserve0=reserve0,
-            reserve1=reserve1
-        )
-        estimate_volume(swap, db)
-        db.serialize(swap)
-        db.discover_dex_pool(swap)
+            swap = DexSwapParsed(
+                tx_hash=Parser.require(obj.get('tx_hash', None)),
+                msg_hash=Parser.require(obj.get('msg_hash', None)),
+                trace_id=Parser.require(obj.get('trace_id', None)),
+                platform=DEX_BIDASK_CLMM,
+                swap_utime=Parser.require(obj.get('created_at', None)),
+                swap_user=input_user_address,
+                swap_pool=Parser.require(obj.get('destination', None)),
+                swap_src_token=src_token_master,
+                swap_dst_token=dst_token_master,
+                swap_src_amount=input_amount,
+                swap_dst_amount=output_amount,
+                referral_address=ref_addr,
+                query_id=input_query_id
+            )
+
+            estimate_volume(swap, db)
+            db.serialize(swap)
+            db.discover_dex_pool(swap)
+        elif op == 0xd3a25890: # pool:swap_success_callback_v2
+            query_id = cell.load_uint(64)
+            new_current_bin_number = cell.load_uint(32)
+            new_sqrt_p = cell.load_uint(32)
+            from_account = cell.load_bit()
+            amount_x = cell.load_coins()
+            amount_y = cell.load_coins()
+            is_x = cell.load_bit()
+            receiver_address = cell.load_address()
+            ref_cell = cell.load_maybe_ref()
+            ref_addr = None
+            if ref_cell is not None:
+                ref_addr = ref_addr.begin_parse().load_address()
+
+
+            logger.info(f"query_id: {query_id}, from_account: {from_account}, amount_x: {amount_x}, amount_y: {amount_y}, is_x: {is_x}, receiver_address: {receiver_address}, from_address: {from_address}")
+
+            range_tx = obj
+            for i in range(5): # max 5 reentrancies for swap
+                try:
+                    range_swap = Cell.one_from_boc(db.get_parent_message_body(range_tx.get('msg_hash'))).begin_parse()
+                except Exception as e:
+                    logger.info("parrent range swap not found", e)
+
+                swap_op = range_swap.load_uint(32)
+                if swap_op != 0x87d36990: # range:swap_v2
+                    logger.warning(f"Parent message for bidask clmm swap is {swap_op}, expected 0x66210c65 {tx_hash}")
+                    range_tx = db.get_parent_message_with_body(range_tx.get('msg_hash'))
+                    continue
+                else:
+                    break
+
+            input_query_id = range_swap.load_uint(64)
+            input_user_address = range_swap.load_address()
+            input_is_account = range_swap.load_bit()
+            input_is_x = range_swap.load_bit()
+            input_amount = range_swap.load_coins()
+            input_out = range_swap.load_coins()
+
+            logger.info(f"input_query_id: {input_query_id}, input_is_account: {input_is_account}, input_is_x: {input_is_x}, input_amount: {input_amount}, is_x: {is_x}, input_out: {input_out}")
+
+            token_x, token_y, = self._execute_method(pool_emulator, 'get_pool_tokens', [], db, {})
+            token_x_address = token_x.load_address()
+            token_y_address = token_y.load_address()
+
+            src_token = [token_x_address, token_y_address][is_x]
+            dst_token = [token_y_address, token_x_address][is_x]
+            output_amount = [amount_x, amount_y][is_x]
+
+            src_token_master = db.get_wallet_master(src_token)
+            dst_token_master = db.get_wallet_master(dst_token)
+
+            swap = DexSwapParsed(
+                tx_hash=Parser.require(obj.get('tx_hash', None)),
+                msg_hash=Parser.require(obj.get('msg_hash', None)),
+                trace_id=Parser.require(obj.get('trace_id', None)),
+                platform=DEX_BIDASK_CLMM,
+                swap_utime=Parser.require(obj.get('created_at', None)),
+                swap_user=input_user_address,
+                swap_pool=Parser.require(obj.get('destination', None)),
+                swap_src_token=src_token_master,
+                swap_dst_token=dst_token_master,
+                swap_src_amount=input_amount,
+                swap_dst_amount=output_amount,
+                referral_address=ref_addr,
+                query_id=input_query_id  
+            )
+
+            estimate_volume(swap, db)
+            db.serialize(swap)
+            db.discover_dex_pool(swap)
+        
 
